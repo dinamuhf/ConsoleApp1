170f516a6894c5f15cedd89deaf1a8c656e589f9
dina Muhammed farghly <dinamuhammed2001@gmail.com>
2025-07-02 22:55:35 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿#region 1
+namespace EnumExample
+{
+    class Program
+    {
+        enum WeekDays
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Days of the Week:");
+            foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+            {
+                Console.WriteLine(day);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+#endregion 
